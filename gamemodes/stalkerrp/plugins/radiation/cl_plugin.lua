e0490787a846d1ccb2f00b307a5a638759611955
ScratKnapp <hatdescript@yahoo.ca>
2023-06-30 11:16:00 -0500

    Add da rest of rad changes

--- gamemodes/stalkerrp/plugins/radiation/cl_plugin.lua
+++ gamemodes/stalkerrp/plugins/radiation/cl_plugin.lua
@@ -2,28 +2,36 @@ local rad = Material("daui/rad_icon_v6.png")
 
 function PLUGIN:HUDPaint()
 	local lp = LocalPlayer()
-	local wep = LocalPlayer():GetActiveWeapon()
 	local char = lp:GetCharacter()
 	if (!lp:GetCharacter() or !lp:Alive() or ix.gui.characterMenu:IsVisible()) then return end
 	local rad = Material("vgui/hud/rad.png", "noclamp smooth") 
 	local rad2 = Material("vgui/hud/rad2.png", "noclamp smooth") 
 	local rad3 = Material("vgui/hud/rad3.png", "noclamp smooth") 
 	local rad4 = Material("vgui/hud/rad4.png", "noclamp smooth")
-
 	surface.SetMaterial(rad)
-	if LocalPlayer():getRadiation() == 0 then
+
+		-- Anything below 200 rads will not show up on HUD or any effects.
+	if char:GetRads() < 199  then
 		surface.SetMaterial(rad)
 		surface.SetDrawColor(Color(0, 0, 0, 0))
-	elseif LocalPlayer():getRadiation() > 0 and LocalPlayer():getRadiation() <= 25 then
+
+		-- 200-399 Rads: Minor Rad Poisoning
+	elseif char:GetRads() > 199 and char:GetRads() < 400 then
 		surface.SetMaterial(rad)
 		surface.SetDrawColor(Color(255, 255, 255, 255))
-	elseif LocalPlayer():getRadiation() > 25 and LocalPlayer():getRadiation() <= 45 then
+
+		-- 400-599 Rads: Advanced Rad Poisoning
+	elseif char:GetRads() > 399 and char:GetRads() < 600 then
 		surface.SetMaterial(rad2)
 		surface.SetDrawColor(Color(255, 255, 255, 255))
-	elseif LocalPlayer():getRadiation() > 45 and LocalPlayer():getRadiation() <= 75 then
+
+		-- 600-799 Rads: Critical Rad Poisoning
+	elseif char:GetRads() > 599 and char:GetRads() < 800 then
 		surface.SetMaterial(rad3)
 		surface.SetDrawColor(Color(255, 255, 255, 255))
-	elseif LocalPlayer():getRadiation() > 75 and LocalPlayer():getRadiation() <= 100 then
+
+		-- 800+ Rads: Deadly Rad Poisoning
+	elseif char:GetRads() >= 800 then
 		surface.SetMaterial(rad4)
 		surface.SetDrawColor(Color(255, 255, 255, 255))
 	end
--- gamemodes/stalkerrp/plugins/radiation/sh_plugin.lua
+++ gamemodes/stalkerrp/plugins/radiation/sh_plugin.lua
@@ -1,6 +1,6 @@
 local PLUGIN = PLUGIN
 PLUGIN.name = "Radiation"
-PLUGIN.author = "Some body onc-"
+PLUGIN.author = "Modified by Scrat"
 PLUGIN.desc = "Radiation System"
 
 ix.util.Include("cl_plugin.lua")
@@ -8,24 +8,152 @@ ix.util.Include("cl_plugin.lua")
 local playerMeta = FindMetaTable("Player")
 local entityMeta = FindMetaTable("Entity")
 
-function playerMeta:getRadiation()
-	return (self:GetNetVar("AccumRads")) or 0
+ix.char.RegisterVar("Rads", {
+    field = "rads",
+    fieldType = ix.type.number,
+    default = 0,
+    isLocal = true,
+    bNoDisplay = true
+})
+
+ix.char.RegisterVar("RadImmune", {
+    field = "radimmune",
+    fieldType = ix.type.boolean,
+    default = false,
+    isLocal = true,
+    bNoDisplay = true
+})
+
+function playerMeta:GetRads()
+	return self:GetCharacter():GetRads()
 end
 
-function playerMeta:getRadiationPercent()
-	return math.Clamp(self:getRadiation()/100, 0 ,1)
+function playerMeta:GetRadsPercent()
+	return math.Clamp(self:GetCharacter():GetRads()/100, 0 ,1)
 end
 
 function playerMeta:addRadiation(amount)
-	local curRadiation = self:getRadiation()
+	local curRadiation = self:GetCharacter():GetRads()
+	self:setRadiation(math.Clamp(math.min(curRadiation) + amount, 0, 1000))
+end
 
-	self:SetNetVar("AccumRads", 
-		math.Clamp(math.min(curRadiation) + amount, 0, 100)
-	)
+
+function playerMeta:radDebuffHandler(debuff)
+	char = self:GetCharacter()
+	-- Handle all buffs/debuffs here and call in each rad level check further down.
+	if debuff == "NONE" then
+		char:RemoveBoost("minorrad", "fortitude")
+
+		char:RemoveBoost("advancedrad", "fortitude")
+		char:RemoveBoost("advancedrad", "reflex")
+
+		char:RemoveBoost("criticalrad", "fortitude")
+		char:RemoveBoost("criticalrad", "reflex")
+		char:RemoveBoost("criticalrad", "strength")
+
+		char:RemoveBoost("deadlyrad", "fortitude")
+		char:RemoveBoost("deadlyrad", "reflex")
+		char:RemoveBoost("deadlyrad", "strength")
+	end
+
+	if debuff == "MINOR" then
+		char:AddBoost("minorrad", "fortitude", -1)
+
+		char:RemoveBoost("advancedrad", "fortitude")
+		char:RemoveBoost("advancedrad", "reflex")
+
+		char:RemoveBoost("criticalrad", "fortitude")
+		char:RemoveBoost("criticalrad", "reflex")
+		char:RemoveBoost("criticalrad", "strength")
+
+		char:RemoveBoost("deadlyrad", "fortitude")
+		char:RemoveBoost("deadlyrad", "reflex")
+		char:RemoveBoost("deadlyrad", "strength")
+	end
+
+	if debuff == "ADVANCED" then
+		char:RemoveBoost("minorrad", "fortitude")
+
+		char:AddBoost("advancedrad", "fortitude", -2)
+		char:AddBoost("advancedrad", "reflex", -1)
+
+		char:RemoveBoost("criticalrad", "fortitude")
+		char:RemoveBoost("criticalrad", "reflex")
+		char:RemoveBoost("criticalrad", "strength")
+
+		char:RemoveBoost("deadlyrad", "fortitude")
+		char:RemoveBoost("deadlyrad", "reflex")
+		char:RemoveBoost("deadlyrad", "strength")
+	end
+
+	if debuff == "CRITICAL" then
+		char:RemoveBoost("minorrad", "fortitude")
+
+		char:RemoveBoost("advancedrad", "fortitude")
+		char:RemoveBoost("advancedrad", "reflex")
+
+		char:AddBoost("criticalrad", "fortitude", -3)
+		char:AddBoost("criticalrad", "reflex", -2)
+		char:AddBoost("criticalrad", "strength", -1)
+
+		char:RemoveBoost("deadlyrad", "fortitude")
+		char:RemoveBoost("deadlyrad", "reflex")
+		char:RemoveBoost("deadlyrad", "strength")
+	end
+
+	if debuff == "DEADLY" then
+		char:RemoveBoost("minorrad", "fortitude")
+
+		char:RemoveBoost("advancedrad", "fortitude")
+		char:RemoveBoost("advancedrad", "reflex")
+
+		char:RemoveBoost("criticalrad", "fortitude")
+		char:RemoveBoost("criticalrad", "reflex")
+		char:RemoveBoost("criticalrad", "strength")
+
+		char:AddBoost("deadlyrad", "fortitude", -3)
+		char:AddBoost("deadlyrad", "reflex", -2)
+		char:AddBoost("deadlyrad", "strength", -2)
+	end
+
+
 end
 
 function playerMeta:setRadiation(amount)
-	self:SetNetVar("AccumRads", math.Clamp(amount, 0, 100))
+	local char = self:GetCharacter()
+
+
+	if char:GetRadImmune() == true then
+		amount = 0
+	end
+
+	char:SetRads((math.Clamp(amount, 0, 1000)))
+
+		-- Less than 200 rads, no effects, make sure to remove all debuffs related to rad posioning.
+	if char:GetRads() < 200 then
+		self:radDebuffHandler("NONE")
+
+		-- 200-399 Rads: Minor Rad Poisoning, -1 END
+	elseif char:GetRads() > 199 and char:GetRads() < 400 then
+		self:radDebuffHandler("MINOR")
+		char:GetPlayer():Notify("You are sick with Minor Radiation Poisoning.")
+
+		-- 400-599 Rads: Advanced Rad Poisoning, -2 END, -1 AGI
+	elseif char:GetRads() > 399 and char:GetRads() < 600 then
+		self:radDebuffHandler("ADVANCED")
+		char:GetPlayer():Notify("You are sick with Advanced Radiation Poisoning.")
+
+		-- 600-799 Rads: Critical Rad Poisoning, -3 END, -2 AGI, -1 STR
+	elseif char:GetRads() > 599 and char:GetRads() < 800 then
+		self:radDebuffHandler("CRITICAL")
+		char:GetPlayer():Notify("You are sick with Critical Radiation Poisoning.")
+
+		-- 800+ Rads: Deadly Rad Poisoning -3 END, -2 AGI, -2 STR
+	elseif char:GetRads() >= 800 then
+		self:radDebuffHandler("DEADLY")
+		char:GetPlayer():Notify("You are sick with Deadly Radiation Poisoning.")
+	end
+
 end
 
 function playerMeta:hasGeiger()
@@ -45,97 +173,121 @@ function PLUGIN:PostPlayerLoadout(client)
 	end
 end
 
-function playerMeta:getRadResist()
-	local res = 0
-	local char = self:GetCharacter()
-	local items = char:GetInventory():GetItems(true)
 
-	for k,v in pairs(items) do
-		if (v.radProt and v:GetData("equip") == true) then
-			res = res + v.radProt
-			break
-		end
-	end
 
-	--second loop for artifacts
 
-	if ix.plugin.list["buffs"] then
-		if self:HasBuff("buff_radprotect") then
-			res = res + 0.1
-		end
-	end
-
-	return res
-end
-
-function PLUGIN:EntityTakeDamage(entity, dmgInfo)
-	--RADIATION OVERRIDE
-	if ( entity:IsPlayer() and dmgInfo:IsDamageType(DMG_RADIATION)) then
-		local radAmount = (dmgInfo:GetDamage()/10)
-		local radResist = entity:getRadResist()
-		
-		entity:addRadiation(math.Clamp(radAmount - radResist ,0, 100))
-		dmgInfo:SetDamage(0)
-	end
-end
 
 -- Register HUD Bars.
 if (CLIENT) then
+	local char = LocalPlayer():GetCharacter()
 	local color = Color(39, 174, 96)
-
-	--nut.bar.add(function() return (LocalPlayer():getRadiationPercent()) end, color, nil, "radiation")
 	function PLUGIN:RenderScreenspaceEffects()
-		if (LocalPlayer():getRadiation() > 45 and LocalPlayer():getRadiation() < 75) then
-			DrawMotionBlur(0.05, 0.15, 0.001)
-		elseif(LocalPlayer():getRadiation() > 75) then
-			DrawMotionBlur(0.1, 0.3, 0.001)
+			-- Advanced Poisoning.
+		if char:GetRads() > 399 and char:GetRads() < 600 then
+			DrawMotionBlur(0.05, 0.25, 0.001)
+
+			-- Critical Poisoning.
+		elseif char:GetRads() > 599 and char:GetRads() < 800 then
+			DrawMotionBlur(0.05, 0.25, 0.001)
+
+			-- Deadly Poisoning.
+		elseif char:GetRads() >= 800 then
+			DrawMotionBlur(0.1, 0.35, 0.001)
 		end
     end
 else
 	local PLUGIN = PLUGIN
 	
 	function PLUGIN:CharacterPreSave(character)
-		local savedRads = math.Clamp(character.player:getRadiation(), 0, 100)
+		local savedRads = math.Clamp(character.player:GetRads(), 0, 1000)
 		character:SetData("radiation", savedRads)
 	end
 
 	function PLUGIN:PostPlayerLoadout(client)
-		if (client:GetCharacter():GetData("radiation")) then
-			client:SetNetVar("AccumRads", client:GetCharacter():GetData("radiation"))
+		if (client:GetCharacter():GetRads()) then
+			client:GetCharacter():SetRads(client:GetRads())
 		else
-			client:SetNetVar("AccumRads", 0)
+			client:SetRads(0)
 		end
 	end
 
 	function PLUGIN:PlayerDeath(client)
-		client.resetRads = true
+		client.resetRads = false
 	end
 
 	function PLUGIN:PlayerSpawn(client)
 		if (client.resetRads) then
-			client:SetNetVar("AccumRads", 0)
+			client:SetRads(0)
 			client.resetRads = false
 		end
 	end
 end
 
-ix.command.Add("charsetradiation", {
+ix.command.Add("CharSetRadiation", {
 	adminOnly = true,
 	arguments = {
-		ix.type.string,
+		ix.type.character,
 		ix.type.number,
 	},
 	OnRun = function(self, client, target, radiation)
-		local target = ix.util.FindPlayer(target)
 		local radiation = tonumber(radiation)
 
-		target:setRadiation(radiation)
+		if target:GetRadImmune() == true then
+			return client:Notify(target:GetName() .. " is set to be immune to radiation.")
+		end
 
-		if client == target then
+		target:GetPlayer():setRadiation(radiation)
+
+		if client == target:GetPlayer() then
             client:Notify("You have set your radiation to "..radiation)
         else
-            client:Notify("You have set "..target:Name().."'s radiation to "..radiation)
+            client:Notify("You have set "..target:GetName().."'s radiation to "..radiation)
             target:Notify(client:Name().." has set your radiation to "..radiation)
         end
+	end
+})
+
+ix.command.Add("CharAddRadiation", {
+	adminOnly = true,
+	arguments = {
+		ix.type.character,
+		ix.type.number,
+	},
+	OnRun = function(self, client, target, radiation)
+		local radiation = tonumber(radiation)
+
+		if target:GetRadImmune() == true then
+			return client:Notify(target:GetName() .. " is set to be immune to radiation.")
+		end
+
+		target:GetPlayer():addRadiation(radiation)
+
+		if client == target:GetPlayer() then
+            client:Notify("You have added "..radiation.. " Rads to yourself. You now have " .. target:GetRads() .. " Rads.")
+        else
+            client:Notify("You have added "..radiation.. " to " .. target:GetName() .. ". They now have " .. target:GetRads() .. " Rads.")
+            target:Notify(client:Name().." has added " .. radiation.. "to your Rads. You now have" .. target:GetRads() .. " Rads.")
+        end
+	end
+})
+
+ix.command.Add("CharSetRadImmune", {
+	adminOnly = true,
+	arguments = {
+		ix.type.character,
+		ix.type.bool,
+	},
+	OnRun = function(self, client, target, radimmune)
+		
+		target:SetRadImmune(radimmune)
+		
+		if (target:GetRadImmune() == true) then
+			target:SetRads(0)
+			return client:Notify(target:GetName() .. "has been set to be immune to radiation.")
+		end
+
+		if (target:GetRadImmune() == false) then
+			return client:Notify(target:GetName() .. "has been set to be vulnerable to radiation.")
+		end
 	end
 })
