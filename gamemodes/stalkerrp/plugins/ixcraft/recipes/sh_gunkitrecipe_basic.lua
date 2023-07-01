88b2ca1cbcd5a95f65440a26fb9f15b45625d8b0
Sebastionate <51052308+Sebastionate@users.noreply.github.com>
2023-06-30 19:10:25 -0500

    Revert

---
+++ gamemodes/stalkerrp/plugins/ixcraft/recipes/sh_gunkitrecipe_basic.lua
@@ -0,0 +1,20 @@
+RECIPE.name = "Basic Gunkit Example Recipe"
+RECIPE.description = "Sample recipe for Basic Kit Gunsmiths"
+RECIPE.model = "models/jerry/headtorch.mdl"
+RECIPE.category = "Basic Gunsmithing"
+RECIPE.toolkits = {
+    "gunkitbasic",
+    "gunkitadvanced",
+    "gunkitexpert"
+}
+RECIPE.flag = "7"
+
+RECIPE.requirements = {
+	["bulb"] = 1,
+["brokenheadlamp"] = 1,
+}
+
+RECIPE.results = {
+	["headlamp"] = 1
+
+}
