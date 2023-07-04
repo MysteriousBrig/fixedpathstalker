8239f9077056fb4141baaddc0449e01377a33575
ScratKnapp <hatdescript@yahoo.ca>
2023-07-04 15:38:03 -0500

    Artifact buffs test complete, added example artifact

--- gamemodes/stalkerrp/plugins/artifacts/items/artifacts/sh_stattest.lua
+++ gamemodes/stalkerrp/plugins/artifacts/items/artifacts/sh_stattest.lua
@@ -1,4 +1,4 @@
-ITEM.name = "Absolve"
+ITEM.name = "Stat Test"
 ITEM.model = "models/tnb/stalker/artifacts/sponge.mdl"
 ITEM.description = "A flat fungus-like organism."
 ITEM.longdesc = "Often found in bodies of water predominantly in the Lost Highway. It uses some form of bio-electric process to purify the water and in this way is able to absorb considerable electricity, but those wearing it will find their body more fragile and subject to bleeding more easily.\n\nBleeding +2\nElectrical +4\nImpact -1"
@@ -17,4 +17,11 @@ ITEM.res = {
 	["Radiation"] = 0.00,
 	["Chemical"] = 0.00,
 	["Psi"] = 0.00,
+}
+
+
+-- Array of buffs to provide. Valid attribs are strength, reflex, fortune, observation, and fortitude. Negative number to debuff.
+ITEM.buffs = {
+	["reflex"] = 10,
+	["strength"] = 10
 }
