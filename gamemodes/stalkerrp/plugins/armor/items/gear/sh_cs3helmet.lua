e99e3d7458e5786324fcdc82a0f578bda6930ff4
Sebastionate <51052308+Sebastionate@users.noreply.github.com>
2023-07-05 16:19:57 -0500

    Forgo somethin

--- gamemodes/stalkerrp/plugins/armor/items/gear/sh_cs3helmet.lua
+++ gamemodes/stalkerrp/plugins/armor/items/gear/sh_cs3helmet.lua
@@ -1,8 +1,8 @@
-ITEM.name = "CS-2 Patrol Helmet"
+ITEM.name = "CS-3 Assault Helmet"
 ITEM.description = "A specially designed Clear Sky Helmet"
-ITEM.longdesc = "An armored Helmet originally designed by the long-since eradicated Clear Sky faction. Such a piece of gear is rumored to be coated in a specially engineered Artifact-Coating, which makes it more resistant to anomaly related Hazards. 
-\n\nBallistic Protection:\nSkull: IIIA\nNeck & Face: II"
-ITEM.model = "models/shtokerbox/ground_headgear_cs2.mdl"
+ITEM.longdesc = "A heavy assault Helmet originally designed by the long-since fallen Clear Sky faction. Such a piece of gear was prominent during the faction's assault on the CNPP, which resulted in their eradication. Said gear piece is also said to be coated in a special cocktail of re-engineered artifacts to offer protection against anomalies and especially Psionic energy."
+\n\nBallistic Protection:\nSkull: III\nNeck & Face: II"
+ITEM.model = "models/shtokerbox/ground_headgear_cs1.mdl"
 ITEM.price = 65000
 ITEM.flag = "3"
 ITEM.br = 0.2
@@ -22,7 +22,7 @@ ITEM.res = {
 	["Bullet"] = 0.00,
 	["Shock"] = 0.00,
 	["Burn"] = 0.00,
-	["Radiation"] = 0.1,
+	["Radiation"] = 0.2,
 	["Chemical"] = 0.1,
-	["Psi"] = 0.2,
+	["Psi"] = 0.4,
 }
