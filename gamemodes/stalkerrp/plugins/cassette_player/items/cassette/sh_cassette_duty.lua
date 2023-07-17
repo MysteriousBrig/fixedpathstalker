709c065203fedb6ff1dbde169a79706d06bfd8a6
ScratKnapp <hatdescript@yahoo.ca>
2023-07-17 13:40:59 -0500

    Musical tapes

--- gamemodes/stalkerrp/plugins/cassette_player/items/cassette/sh_cassette_duty.lua
+++ gamemodes/stalkerrp/plugins/cassette_player/items/cassette/sh_cassette_duty.lua
@@ -6,4 +6,6 @@ ITEM.model = "models/kek1ch/cassette_red1.mdl"
 
 ITEM.cassette_options = {}
 ITEM.cassette_options['anomalyrp/music/faction/duty_base_radio_2.wav'] = {dur = 250}
+ITEM.cassette_options['anomalyrp/music/faction/duty_base_radio_1.wav'] = {dur = 250}
+
 
--- gamemodes/stalkerrp/plugins/cassette_player/items/cassette/sh_cassette_freedom.lua
+++ gamemodes/stalkerrp/plugins/cassette_player/items/cassette/sh_cassette_freedom.lua
@@ -1,11 +1,11 @@
-﻿ITEM.name = "Freedom Cassette Tape"
-ITEM.description = "Green Cassette Tape"
-ITEM.longdesc = "This green cassette tape features songs that you can relax to."
-ITEM.price = 4424
-ITEM.model = "models/kek1ch/cassette_red1.mdl"
+﻿ITEM.name = "Freedom Jingle"
+ITEM.description = "A cassette tape with the Freedom logo spraypainted on it."
+ITEM.longdesc = "This casette contains a relaxed, rasta-inspired instrumental composed by Freedom - as chill and laid back as they are. It's used to help calm the mind after an intense mission."
+ITEM.price = 500
+ITEM.model = "models/kek1ch/cassette_taksin.mdl"
 
 ITEM.cassette_options = {}
-ITEM.cassette_options['radio/freedm_r_1.ogg'] = {dur = 290}
-ITEM.cassette_options['radio/freedm_r_2.ogg'] = {dur = 111}
+ITEM.cassette_options['anomalyrp/music/faction/freedom_base_radio_1.wav'] = {dur = 250}
+
 
 
