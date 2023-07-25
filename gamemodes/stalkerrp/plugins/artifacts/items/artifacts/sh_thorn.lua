a7e01eb24d380bfc6edf59885d3b3359036815a2
Sebastionate <51052308+Sebastionate@users.noreply.github.com>
2023-07-24 19:43:14 -0500

    Update

--- gamemodes/stalkerrp/plugins/artifacts/items/artifacts/sh_thorn.lua
+++ gamemodes/stalkerrp/plugins/artifacts/items/artifacts/sh_thorn.lua
@@ -1,7 +1,7 @@
 ITEM.name = "Thorn"
 ITEM.model ="models/nasca/etherealsrp_artifacts/crystal_thorn.mdl"
 ITEM.description = "Spikey artifact, but quite small."
-ITEM.longdesc = "Draws out radiation through the use of pricking the user’s skin, no matter if armor is worn or not. When worn will cause bleeding.\n\nRads -2\nBleeding +1"
+ITEM.longdesc = "Draws out radiation through the use of pricking the user’s skin, no matter if armor is worn or not. When worn will cause bleeding.\n\n+1 Chemical\n-3 Bleeding"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.price = 2400
